c6684cabe00faa30b19482f3e2288747a820069d
gento-ogane <ponpon_1325@yahoo.co.jp>
2021-03-03 23:38:34 +0900

    fix

--- parser/parser.go
+++ parser/parser.go
@@ -27,6 +27,7 @@ var precedences = map[token.TokenType]int{
 	token.SLASH:    PRODUCT,
 	token.ASTERISK: PRODUCT,
 	token.LPAREN:   CALL,
+	token.LBRACKET: INDEX,
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -58,6 +59,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
 	p.registerInfix(token.LPAREN, p.parseCallExpression)
+	p.registerInfix(token.LBRACKET, p.parseIndexExpression) //添字演算式の構文解析関数
 
 	//２つのトークンを読み込む
 	p.nextToken()
@@ -201,6 +203,7 @@ const (
 	PRODUCT     //*
 	PREFIX      //-X OR !X
 	CALL        //myFunction(X)
+	INDEX
 )
 
 //式の構文解析関数
@@ -435,4 +438,17 @@ func (p *Parser) parseArrayLiteral() ast.Expression {
 	array := &ast.ArrayLiteral{Token: p.curToken}
 	array.Elements = p.parseExpressionList(token.RBRACKET)
 	return array
+}
+
+//添字演算式の構文解析関数
+func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression {
+	exp := &ast.IndexExpression{Token: p.curToken, Left: left}
+
+	p.nextToken()
+	exp.Index = p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RBRACKET) { //tokenの次に]がなかったらエラー
+		return nil
+	}
+	return exp
 }
